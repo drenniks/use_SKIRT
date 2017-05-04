c03e0809110792286f6ee775f04873d720f5003e
Danielle Skinner <drenniks@Danielles-MacBook-Pro.local>
2017-05-04 16:45:20 -0700

    Adding create.py

---
+++ create.py
@@ -0,0 +1,17 @@
+import os
+import Zubko_maker
+import nodust_maker
+import images_maker
+import images_submit_maker
+import submit_maker
+import master_images_submit
+import master_submit
+
+os.remove('Zubko_maker.pyc')
+os.remove('nodust_maker.pyc')
+os.remove('images_maker.pyc')
+os.remove('images_submit_maker.pyc')
+os.remove('submit_maker.pyc')
+os.remove('master_images_submit.pyc')
+os.remove('master_submit.pyc')
+
