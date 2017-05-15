92b3eca9c6f413773a2c8d1836aeece4a6bb5d4b
Danielle Skinner <drenniks@Danielles-MacBook-Pro.local>
2017-05-14 19:05:26 -0700

    Adding config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+step_number = '008192'
+sim_location = '/oasis/scratch/comet/mjt29/temp_project/Romulus25/cosmo25p.768sg1bwK1BHe75.008192'
+
+#simulation parameters
+#SED instruments:
+detector_count = '2'
+names = ['i00', 'i90']
+distances = ['10 Mpc', '10 Mpc']
+inclinations = ['0 deg', '90 deg']
+azimuths = ['0 deg', '0 deg']
+positionAngles = ['0 deg', '0 deg']
+
+min_wavelength = '0.1 micron'
+max_wavelength = '1000 micron'
+points = '100' 
+
+#Frame instruments:
+frame_count = '3'
+names_img = 'i60, i90, i80'
+distances_img = '10 Mpc, 10 Mpc, 10 Mpc'
+inclinations_img = '60 deg, 90 deg, 80 deg'
+azimuths_img = '0 deg, 0 deg, 0 deg'
+positionAngles_img = '0 deg, 0 deg, 0 deg'
+pixelsX = '800'
+pixelsY = '800'
+fieldOfViewX = '40000 pc'
+fieldOfViewY = '40000 pc'
+
+wavelengths = ['0.45 micron', '0.55 micron', '0.65 micron']
